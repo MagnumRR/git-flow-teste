d672c5e94e24bb3f00e4aa9597343acbcae09429
MAGNUM RIBEIRO RODRIGUES DOS SANTOS <39248308+MagnumRR@users.noreply.github.com>
2025-11-20 17:28:55 -0300

    Git Desenvolvimento - v 1.0.3 - Exibindo em tela

--- calculos.py
+++ calculos.py
@@ -1,14 +1,14 @@
 # TESTES PARA APLICAÇÕES NO GIT
 
-print('-------- CALCULOS --------')
+print('======= CALCULOS ========')
 
-print('Custos Obrigatórios Trabalhistas')
+print('> Custos Obrigatórios Trabalhistas')
 
-print('FGTS (8% do salário bruto mensalmente)')
-print('INSS - 7,5 % do salário bruto')
-print('IRRF - 5% do salário bruto')
+print('* FGTS (8% do salário bruto mensalmente)')
+print('* INSS - 7,5 % do salário bruto')
+print('* IRRF - 5% do salário bruto')
 
-salario = float(input('Informe o salário: '))
+salario = float(input('Informe o salário: R$ '))
 
 def calc_fgts(salario):
     return 0.08 * salario
@@ -16,7 +16,13 @@ def calc_fgts(salario):
 def calc_inss(salario):
     return 0.075 * salario
     
-def calc_IRRF(salario):
+def calc_irrf(salario):
     return 0.05 * salario
 
-    
+total = calc_fgts(salario) + calc_inss(salario) + calc_irrf(salario)
+print()
+print(f'\t Valor do FGTS: R$ {round(calc_fgts(salario),2)}')
+print(f'\t Valor do INSS: R$ {round(calc_inss(salario),2)}')
+print(f'\t Valor do IRRF: R$ {round(calc_irrf(salario),2)}')    
+
+print(f'Total: R$ {round(total,2)}')    
