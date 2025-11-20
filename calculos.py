bf9b7c332b27bd67c2a60cda49c8389893edf1ed
MAGNUM RIBEIRO RODRIGUES DOS SANTOS <39248308+MagnumRR@users.noreply.github.com>
2025-11-20 15:19:52 -0300

    Commit Desenvolvimento - v 1.0.1 - Adição de títulos

--- calculos.py
+++ calculos.py
@@ -1,3 +1,9 @@
 # TESTES PARA APLICAÇÕES NO GIT
 
 print('-------- CALCULOS --------')
+
+print('Custos Obrigatórios Trabalhistas')
+
+print('FGTS (8% do salário bruto mensalmente)')
+print('Adicional de férias (? de um salário bruto)')
+print('13º salário (valor integral do salário)')
