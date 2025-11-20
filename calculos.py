3776704b468475b410b13da89979c5d5c96d3218
MAGNUM RIBEIRO RODRIGUES DOS SANTOS <39248308+MagnumRR@users.noreply.github.com>
2025-11-20 17:10:53 -0300

    Git Desenvolvimento - v 1.0.2 - Criação de funções

--- calculos.py
+++ calculos.py
@@ -5,5 +5,18 @@ print('-------- CALCULOS --------')
 print('Custos Obrigatórios Trabalhistas')
 
 print('FGTS (8% do salário bruto mensalmente)')
-print('Adicional de férias (? de um salário bruto)')
-print('13º salário (valor integral do salário)')
+print('INSS - 7,5 % do salário bruto')
+print('IRRF - 5% do salário bruto')
+
+salario = float(input('Informe o salário: '))
+
+def calc_fgts(salario):
+    return 0.08 * salario
+
+def calc_inss(salario):
+    return 0.075 * salario
+    
+def calc_IRRF(salario):
+    return 0.05 * salario
+
+    
